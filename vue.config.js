1e533178c62ccd6ddf223c92112b7a000d40b134
mikazuki <kagayaki>
2025-05-31 17:44:54 +0900

    Add GitHub Pages deployment settings

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weather-map-vue2/'
+    : '/'
 })
